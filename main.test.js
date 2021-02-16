d40ee662ec70b19a542416829d09584716dddb96
Rick Rieger <rickrieger36251@gmail.com>
2021-02-16 12:45:55 -0500

    javascript complete

--- main.test.js
+++ main.test.js
@@ -1,11 +1,36 @@
 /**********************************
  * YOUR CODE GOES HERE THIS TIME! *
  **********************************/
+let x = 3;
+let y = 10;
+let z = 4;
+let greeting = 'hello';
+let firstName = 'Rick';
+let callStatus = 'ringing';
 
+function double(){
+  x = 2 * x;
+}
 
+function triple(){
+  y = 3 * y;
+}
 
+function square(){
+  z= Math.pow(z,2);
+}
 
+function beNice(){
+  greeting = 'hello' + firstName;
+}
+
+function changeName(){
+  firstName = 'Danger';
+}
  
+function hangUp(){
+  callStatus = 'call ended';
+}
 
 /*********************************************************************************************************************
 
